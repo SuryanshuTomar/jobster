4ad7cedffb5388cbbe0c5aeb807264c4a193e620
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-11-08 22:27:49 +0530

    add: error page structure

--- client/src/pages/utils/Error.tsx
+++ client/src/pages/utils/Error.tsx
@@ -1,14 +1,34 @@
+import type { ErrorResponse } from "react-router-dom";
 import { Link, useRouteError } from "react-router-dom";
 
-const Error = () => {
-	const error = useRouteError();
-	console.log(error);
+import notFound from "../../assets/images/not-found2.svg";
+import somethingWentWrong from "../../assets/images/something-went-wrong.svg";
+import Wrapper from "../../assets/wrappers/ErrorPage";
 
-	return (
-		<div>
-			<h1>Error Page</h1>
-			<Link to="/">Back home</Link>
-		</div>
-	);
+const Error = () => {
+	const error = useRouteError() as ErrorResponse;
+
+	const errorContent =
+		error.status === 404 ? (
+			<Wrapper>
+				<div>
+					<img src={notFound} alt="page not found" />
+					<h3>Ohh! Page not found.</h3>
+					<p>We can't seem to find the page you are looking for!</p>
+					<Link to="/dashboard">Back home</Link>
+				</div>
+			</Wrapper>
+		) : (
+			<Wrapper>
+				<div>
+					<img src={somethingWentWrong} alt="page not found" />
+					<h3>Something went wrong!</h3>
+					<p></p>
+					<Link to="/dashboard">Back home</Link>
+				</div>
+			</Wrapper>
+		);
+
+	return errorContent;
 };
 export default Error;
