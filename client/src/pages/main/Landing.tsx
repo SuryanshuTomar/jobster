524f95c72429801f7cfd8191d18520f40ec0883d
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-11-08 22:09:28 +0530

    add: Landing page structure

--- client/src/pages/main/Landing.tsx
+++ client/src/pages/main/Landing.tsx
@@ -1,4 +1,40 @@
+import { Link } from "react-router-dom";
+
+import main from "../../assets/images/jobster-main.svg";
+import Wrapper from "../../assets/wrappers/LandingPage";
+import Logo from "../../components/Logo";
+
 const Landing = () => {
-  return <div>Landing</div>;
+	return (
+		<Wrapper>
+			<nav>
+				<Logo />
+			</nav>
+			<div className="container page">
+				<div className="info">
+					<h1>
+						Job <span>tracking</span>
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in
+						perferendis eaque odio laudantium, non autem? Ea quas
+						repudiandae sapiente totam labore. Sint architecto aliquid
+						corporis cupiditate placeat debitis ipsam commodi assumenda
+						fugit dolor quibusdam, doloremque quia eligendi maiores non
+						harum eos! Similique voluptatem voluptatibus veniam facere
+						atque quis? At?
+					</p>
+					<Link to="/register" className="btn register-link">
+						Register
+					</Link>{" "}
+					<Link to="/login" className="btn">
+						Login / Demo User
+					</Link>{" "}
+				</div>
+				<img src={main} alt="Job hunt" className="img main-img" />
+			</div>
+		</Wrapper>
+	);
 };
+
 export default Landing;
