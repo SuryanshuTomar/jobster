c2e9bba427ed850974132c2cc7a04ab321c89b7b
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-09 17:58:24 +0530

    add: Register page structure

--- client/src/pages/user/Register.tsx
+++ client/src/pages/user/Register.tsx
@@ -1,4 +1,61 @@
+import { Link } from "react-router-dom";
+
+import Wrapper from "../../assets/wrappers/RegisterAndLoginPage";
+import { Logo } from "../../components";
+import FormRow from "../../components/FormRow";
+
 const Register = () => {
-  return <div>Register</div>;
+	return (
+		<Wrapper>
+			<form className="form">
+				<Logo />
+				<h4>Register</h4>
+				<FormRow
+					type="text"
+					name="firstName"
+					labelText="First Name"
+					defaultValue="John"
+				/>
+				<FormRow
+					type="text"
+					name="lastName"
+					labelText="Last Name"
+					defaultValue="Smith"
+				/>
+				<FormRow
+					type="text"
+					name="location"
+					labelText="Location"
+					defaultValue="Earth"
+				/>
+				<FormRow
+					type="email"
+					name="email"
+					labelText="Email"
+					defaultValue="test@example.com"
+				/>
+				<FormRow
+					type="password"
+					name="password"
+					labelText="Password"
+					defaultValue="Test@123"
+				/>
+				<FormRow
+					type="password"
+					name="confirmPassword"
+					labelText="Confirm Password"
+					defaultValue="Test@123"
+				/>
+				<button className="btn btn-block">Submit</button>
+				<p>
+					Already a member?
+					<Link to="/login" className="member-btn">
+						Login
+					</Link>
+				</p>
+			</form>
+		</Wrapper>
+	);
 };
+
 export default Register;
