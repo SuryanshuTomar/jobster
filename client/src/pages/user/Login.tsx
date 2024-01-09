26f8e42ceff145df113e7aa80a3b80e50ff176b0
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-09 18:58:59 +0530

    add: login page structure

--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -1,4 +1,42 @@
+import { Link } from "react-router-dom";
+
+import Wrapper from "../../assets/wrappers/RegisterAndLoginPage";
+import { Logo } from "../../components";
+import FormRow from "../../components/FormRow";
+
 const Login = () => {
-  return <div>Login</div>;
+	return (
+		<Wrapper>
+			<form className="form">
+				<Logo />
+				<h4>Login</h4>
+				<FormRow
+					type="email"
+					name="email"
+					labelText="Email"
+					defaultValue="test@example.com"
+				/>
+				<FormRow
+					type="password"
+					name="password"
+					labelText="Password"
+					defaultValue="test@123"
+				/>
+				<button type="submit" className="btn btn-block">
+					Submit
+				</button>
+				<button type="button" className="btn btn-block">
+					Explore the App
+				</button>
+
+				<p>
+					Not a member yet?{" "}
+					<Link to="/register" className="member-btn">
+						Register
+					</Link>
+				</p>
+			</form>
+		</Wrapper>
+	);
 };
 export default Login;
