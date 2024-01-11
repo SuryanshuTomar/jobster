d6e57efbba0cde01d253d7988daeca8b700133dc
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-11 13:53:30 +0530

    add: dashboard page structure

--- client/src/pages/layouts/DashboardLayout.tsx
+++ client/src/pages/layouts/DashboardLayout.tsx
@@ -1,6 +1,26 @@
 import { Outlet } from "react-router-dom";
 
+import Wrapper from "../../assets/wrappers/Dashboard";
+import { BigSidebar, Navbar, SmallSidebar } from "../../components";
+import { DashboardContextProvider } from "../../context/DashboardContext";
+
 const DashboardLayout = () => {
-	return <Outlet />;
+	return (
+		<DashboardContextProvider>
+			<Wrapper>
+				<main className="dashboard">
+					<SmallSidebar />
+					<BigSidebar />
+
+					<div>
+						<Navbar />
+						<div className="dashboard-page">
+							<Outlet />
+						</div>
+					</div>
+				</main>
+			</Wrapper>
+		</DashboardContextProvider>
+	);
 };
 export default DashboardLayout;
